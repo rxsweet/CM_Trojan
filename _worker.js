0fe299164f94a6e8c24f949e689e49a3cdb7405c
rxsweet <105535100+rxsweet@users.noreply.github.com>
2024-08-22 17:48:42 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -59,8 +59,8 @@ let RproxyIP = 'false';
 
 // 设置优选地址api接口
 let addressesapi = [
-	'https://raw.githubusercontent.com/rxsweet/CM_Vmess/main/addressesapi.txt',//自用更新ip
-	'https://raw.githubusercontent.com/rxsweet/CM_Vmess/main/addressesapi_自用扫描.txt',//自用扫描IP
+	'https://raw.githubusercontent.com/rxsweet/CM_Trojan/main/addressesapi.txt',//自用更新ip
+	'https://raw.githubusercontent.com/rxsweet/CM_Trojan/main/addressesapi_自用扫描.txt',//自用扫描IP
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
 ];
